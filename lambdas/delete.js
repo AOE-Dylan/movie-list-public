acc217458304b033c63df18098cf6954ded4cc32
gawigwen <19rodnce@waipahuh.k12.hi.us>
2017-12-06 10:55:40 -1000

    initial commit

---
+++ lambdas/delete.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+$('#get').click(function(){
+    $.ajax({
+        url: "https://",
+        type: 'DELETE',
+        success: data => {
+            data.whatever
+            
+        },
+        'Content-Type': 'application/json'
+    })
+});
